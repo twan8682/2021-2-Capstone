cbc857455f612862958dca89ee37c725e2f9ae32
twan8682 <twan8682@naver.com>
2021-10-18 21:44:58 +0900

    파이썬 스트리밍영상 & 차선 탐지 깃허브 소스코드

---
+++ camera/test3.py
@@ -0,0 +1,10 @@
+import cv2
+# 샘플 영상
+url = 'http://192.168.222.242:81/stream'
+cap = cv2.VideoCapture(url)
+
+while True :
+    ret, frame = cap.read()
+    # 윈도우 창 출력용
+    cv2.imshow("1", frame)
+    cv2.waitKey(1)
